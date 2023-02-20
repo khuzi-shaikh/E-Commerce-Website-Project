bfbcacc4143fea7ac8f3cdbf26d5309e93136ae8
khuzi-shaikh <124673301+khuzi-shaikh@users.noreply.github.com>
2023-02-21 01:49:13 +0530

    loader updated

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -1,10 +1,12 @@
+import { CircularProgress } from "@mui/material";
 import React from "react";
 import "./Loader.css"
 
 export const Loader = () => {
   return (
     <div className="spinner-container">
-      <div className="loading-spinner"></div>
+      <CircularProgress />
+      {/* <div className="loading-spinner"></div> */}
     </div>
   );
 };
